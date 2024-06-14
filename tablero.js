81907591aebb1527c1903f84fa9cdbcc67fd8a93
motkvil-dev <motkvil@gmail.com>
2024-06-14 11:23:41 +0200

    hecho diagrama de flujo

---
+++ tablero.js
@@ -0,0 +1,11 @@
+<script>
+ let tablero=[["","",""],["","X",""],["","","X"]];
+ console.log (tablero);
+ tablero[0][0]="X";
+ console.log(tablero); 
+</script>
+
+tauler (coord=>{
+ tauler [parseint](coordenada [o])[parseit](coordenada [X]);
+ console.log (tauler)
+})
